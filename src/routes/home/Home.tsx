e5234bcc1e8f5e55d1ec08fea52e6cfb88a68875
Rob Doneux <robertdoneux@gmail.com>
2024-10-11 15:04:58 +0100

    feat/tailwind: adds main admin navigation page

--- src/routes/home/Home.tsx
+++ src/routes/home/Home.tsx
@@ -1,9 +1,23 @@
 import styles from './Home.module.css';
+import Fingerprint from './fragments/Fingerprint';
+import HomeNavigationButton from './fragments/HomeNavigationButton';
+import Quill from './fragments/Quill';
 
 export default function Home() {
   return (
     <div className={styles['container']}>
-      <h1>TPLT | WEB SPA</h1>
+      <h1 className="text-5xl">MSLF | ADMIN</h1>
+      <ul className="grid grid-cols-2 gap-16 justify-items-center self-start">
+        <HomeNavigationButton
+          title="Update Details"
+          route="/update-information"
+        >
+          <Fingerprint />
+        </HomeNavigationButton>
+        <HomeNavigationButton title="Edit Poem" route="/edit-poem">
+          <Quill />
+        </HomeNavigationButton>
+      </ul>
     </div>
   );
 }
---
+++ src/routes/update-information/UpdateInformation.tsx
@@ -0,0 +1,3 @@
+export default function UpdateInformation() {
+  return <p>UpdateInformation works!</p>
+}
