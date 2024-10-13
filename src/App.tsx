9784dfcf2e84fafa93af3ca470efd92851c1b4a3
Rob Doneux <robertdoneux@gmail.com>
2024-10-13 20:20:56 +0100

    feat/authentication: adds login page

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,22 @@ import UpdateInformation from './routes/update-information/UpdateInformation';
 import { Toaster } from 'react-hot-toast';
 import Poems from './routes/poems/Poems';
 import CreatePoem from './routes/create-poem/CreatePoem';
+import IsAuthenticated from './authentication/IsAuthenticated';
+import Login from './authentication/Login';
 
 function App() {
   document.documentElement.classList.add('dark');
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element={<Home />} />
-        <Route path="/update-information" element={<UpdateInformation />} />
-        <Route path="/poems" element={<Poems />} />
-        <Route path="/create-poem" element={<CreatePoem />} />
-        <Route path="/create-poem/:id" element={<CreatePoem />} />
+        <Route path="/login" element={<Login />} />
+        <Route element={<IsAuthenticated />}>
+          <Route index element={<Home />} />
+          <Route path="/update-information" element={<UpdateInformation />} />
+          <Route path="/poems" element={<Poems />} />
+          <Route path="/create-poem" element={<CreatePoem />} />
+          <Route path="/create-poem/:id" element={<CreatePoem />} />
+        </Route>
         <Route path="*" element={<NotFound />} />
       </Routes>
       <Toaster />
---
+++ src/authentication/Login.tsx
@@ -0,0 +1,65 @@
+import { useEffect, useState } from 'react';
+import { auth } from '../firebase';
+import { useNavigate } from 'react-router-dom';
+import MslfInput from '../components/MslfInput';
+import { Button } from '@material-tailwind/react';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+
+export default function Login() {
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [error, setError] = useState<string>('');
+
+  useEffect(() => {
+    // Check if the user is already logged in
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        navigate('/'); // Redirect if logged in
+      }
+    });
+
+    return () => unsubscribe();
+  }, [navigate]);
+
+  async function onLogin() {
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      navigate('/');
+    } catch (error: any) {
+      setError(error.message);
+    }
+  }
+
+  return (
+    <div className="w-full h-full flex items-center justify-center flex-col">
+      <h1 className="text-5xl ">Login</h1>
+      <form className="grid gap-5 w-1/3 pt-5">
+        <MslfInput
+          onChange={(e) => setEmail(e)}
+          value={email}
+          label="Email"
+          type="email"
+        />
+        <MslfInput
+          onChange={(e) => setPassword(e)}
+          value={password}
+          label="Password"
+          type="password"
+        />
+        <Button
+          className="rounded h-[35px]"
+          color="orange"
+          onClick={onLogin}
+          placeholder={undefined}
+          onPointerEnterCapture={undefined}
+          onPointerLeaveCapture={undefined}
+        >
+          Submit
+        </Button>
+        {error ? <small>{error}</small> : null}
+      </form>
+    </div>
+  );
+}
