2a6ed92a741792efd9dde9568b939d4b6687198b
Rob Doneux <robertdoneux@gmail.com>
2024-10-14 15:50:54 +0100

    fix: updates product name

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,11 +1,11 @@
 import styles from './Footer.module.css';
-import { version } from '../../../package.json';
+import { version, name } from '../../../package.json';
 import Logout from '../../authentication/Logout';
 
 export default function Footer() {
   return (
     <footer className={styles['footer']}>
-      <span className={`${styles['app-title']} dots`}>Myself Admin</span>
+      <span className={`${styles['app-title']} dots`}>{name}</span>
       <span className={styles['app-version']}>App Version: {version}</span>
       <Logout />
     </footer>
